a8ac4f38a90c177d79f5980be289be9eaeec215b
Tahaa <bukharitahaali@gmail.com>
2026-05-21 04:20:44 +0500

    new ci cd pipeline + rm dupe app

--- app/main/java/com/bimarihaunter/ui/theme/Theme.kt
+++
@@ -1,9 +0,0 @@
-package com.bimarihaunter.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val MidnightBlack = Color(0xFF0D0D0D)
-val CardBackground = Color(0xFF1A1A1A)
-val AccentGreen = Color(0xFF4ADE80)
-val DangerRed = Color(0xFFEF4444)
-val WarningOrange = Color(0xFFF97316)
